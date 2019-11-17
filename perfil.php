83ba0725d32a9ceea38eafe0ed7e5359fc3de370
vitorhugoinf <40009988+vitorhugoinf@users.noreply.github.com>
2019-11-17 17:35:05 -0300

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -11,20 +11,25 @@
 <!DOCTYPE html>
 <html>
   <head>
+      <title>Perfil</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="perfil.php">
+   <link href="perfil.css" rel="stylesheet">
+   <link rel="icon" type="imagem/png" href="imgs/icone.png">
     </head>
 <body>
-  <h1>perfil</h1>
-  <p>NOME:<?php echo $_SESSION['name'];?></p>
+<div id="perfil">
+<h1><img src="https://fontmeme.com/permalink/191117/0b9dbc243eb780cdd7dd994c0fc8b5f9.png" alt="super-mario-font"></h1>
+  <p><img src="https://fontmeme.com/permalink/191117/c8d9c1893d8dd211ababa60bb9c3b3e9.png" alt="super-mario-font"> <?php echo $_SESSION['name'];?></p>
   <?php
   $consulta="SELECT * from vitor WHERE usuario='$nome'";
   $resultadp=mysqli_query($bd,$consulta);
       foreach ($resultadp as $result){
         ?>
-          <p>EMAIL<?php echo $result["email"];?></p>
+          <p><img src="https://fontmeme.com/permalink/191117/11e9e93267f7031ec3b2f09a0d535827.png" alt="super-mario-font"> <?php echo $result["email"];?></p>
         <?php }
       ?>
+      </div>
     </body>
 </html>
