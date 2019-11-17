a44a08c2199bd7dab2fc73cc3d411a8748e6d737
vitorhugoinf <40009988+vitorhugoinf@users.noreply.github.com>
2019-11-17 13:42:07 -0300

    Add files via upload

--- jogo/p20.js
+++ jogo/p20.js
@@ -1,7 +1,7 @@
 let config={
     type:Phaser.AUTO,
-    width:900,
-    height:600,
+    width:800,
+    height:700,
     physics:{
         default:'arcade',
         arcade:{
@@ -15,65 +15,62 @@ let config={
         update:update,
     }
 }
-let jogador,movimentos,tijo,ponto,maca,inimigo;
+let jogador,movimentos,mesa,ponto,maca,inimigo,quadroDepontuacao,pontuacao=0;
 var Game=new Phaser.Game(config);
 function preload(){
     this.load.image('player','/img/hasanand.png');
-    this.load.image('fundo','/img/fundo.jpg');
-    this.load.image('bloco','/img/bloco.png');
-    this.load.image('maca','/img/maca.png');
+    this.load.image('fundo','/img/fundop20.png');
+    this.load.image('mesa','/img/mesaa.png');
+    this.load.image('maca','/img/caderno2.png');
     this.load.image('inimigo','/img/freira.png');
     this.load.image('cuidado','/img/caution.png')
 }
 function create(){
     this.add.image(400,300,'fundo');
-    tijo=this.physics.add.staticGroup();
+    mesa=this.physics.add.staticGroup();
     placa=this.physics.add.staticGroup();
-    tijo.create(100,100,'bloco');
-    tijo.create(100,200,'bloco');
-    placa.create(175,250,'cuidado')
-    tijo.create(100,300,'bloco');
-    tijo.create(100,400,'bloco');
-    tijo.create(100,500,'bloco');
-    //segunda fileira de bloquinhos
-    tijo.create(250,100,'bloco');
-    tijo.create(250,200,'bloco');
-    tijo.create(250,300,'bloco');
-    tijo.create(250,400,'bloco');
-    tijo.create(250,500,'bloco');
-    //terceira fileira
-    tijo.create(400,100,'bloco');
-    tijo.create(400,200,'bloco');
-    tijo.create(400,300,'bloco');
-    tijo.create(400,400,'bloco');
-    tijo.create(400,500,'bloco');
-    //quarta fileira
-    tijo.create(550,100,'bloco');
-    tijo.create(550,200,'bloco');
-    tijo.create(550,300,'bloco');
-    tijo.create(550,400,'bloco');
-    tijo.create(550,500,'bloco');
-    //quinta fileira
-    tijo.create(700,100,'bloco');
-    tijo.create(700,200,'bloco');
-    tijo.create(700,300,'bloco');
-    tijo.create(700,400,'bloco');
-    tijo.create(700,500,'bloco');
     maca=this.physics.add.staticGroup();
-    maca.create(700,550,'maca');
-            maca.children.iterate(function(child){
-        child.setBounceX(Phaser.Math.FloatBetween(0.4,0.8));
-    });
+    mesa.create(100,100,'mesa');
+    placa.create(175,450,'cuidado');
+    mesa.create(100,200,'mesa');
+    placa.create(330,200,'cuidado');
+    mesa.create(100,300,'mesa');
+    mesa.create(100,400,'mesa');
+    mesa.create(100,500,'mesa');
+    //segunda fileira de bloquinhos
+    mesa.create(250,100,'mesa');
+    mesa.create(250,200,'mesa');
+    mesa.create(250,300,'mesa');
+    mesa.create(250,400,'mesa');
+    mesa.create(250,500,'mesa');
+    placa.create(395,250,'cuidado');
+    //terceira fileira
+    mesa.create(400,100,'mesa');
+    mesa.create(400,200,'mesa');
+    mesa.create(400,300,'mesa');
+    mesa.create(400,400,'mesa');
+    mesa.create(400,500,'mesa');
+    //quarta fileira
+    mesa.create(550,100,'mesa');
+    mesa.create(550,200,'mesa');
+    mesa.create(550,300,'mesa');
+    mesa.create(550,400,'mesa');
+    mesa.create(550,500,'mesa');
+    //quinta fileira
+    mesa.create(700,100,'mesa');
+    mesa.create(700,200,'mesa');
+    mesa.create(700,300,'mesa');
+    mesa.create(700,400,'mesa');
+    placa.create(700,150,'cuidado');
+    mesa.create(700,500,'mesa');
+    maca.create(765,565,'maca');
     //maca.create(750,560,'maca');
     jogador=this.physics.add.sprite(500,500,'player');
     jogador.setCollideWorldBounds(true);
     inimigo=this.physics.add.sprite(100,455,'inimigo');
     inimigo.enableBody = true;
     inimigo.setCollideWorldBounds(true);
-    //inimigo.create(600,700,'inimigo');
-    //inimigo.setCollideWorldBounds(true);
     
-    this.physics.add.collider(jogador,tijo);
     this.anims.create({
         key:'left',
         frames:[{key:'player',frame:0}],
@@ -123,71 +120,68 @@ function create(){
         frameRate: 10,
         repeat: -1
     });
+    quadroDepontuacao=this.add.text(16,650,'Pontuação: 0',{
+        fontSize:'32px',
+        fill:'#000'     
+   });
     movimentos=this.input.keyboard.createCursorKeys();
-    
-
-    //this.physics.add.collider(jogador,inimigo,aproximaInimigo);
-    this.physics.add.collider(tijo,inimigo,aproximaInimigo);
+    this.physics.add.collider(jogador,mesa);
     this.physics.add.collider(placa,inimigo);
     this.physics.add.collider(placa,jogador);
     this.physics.add.overlap(maca,jogador,contadorDeponto,null,this);
+    this.physics.add.overlap(inimigo,jogador,perde,null,this);
+    this.physics.add.collider(mesa,inimigo);
     }
-    
-    
     function update(){
         if(movimentos.left.isDown){
             jogador.setVelocityX(-260);
-            jogador.anims.play('left',true);
-          //  inimigo.setVelocityX(-160);
-         //   inimigo.anims.play('left',true);//adcionando a animação 
+            jogador.anims.play('left',true); 
          }
       else if(movimentos.right.isDown){
         jogador.setVelocityX(260);
         jogador.anims.play('right',true);
-        //inimigo.setVelocityX(160);
-        //inimigo.anims.play('right',true);//adcionando a animação
         }
          else{
            jogador.setVelocityX(0);
-      //     inimigo.setVelocityX(0);
           }  
      if(movimentos.up.isDown){
             jogador.setVelocityY(-260);
-          //  inimigo.setVelocityY(-160);
       }
       else if(movimentos.down.isDown){
         jogador.setVelocityY(260);
-        //inimigo.setVelocityY(160);
+      }
+      else{
+        jogador.setVelocityY(0);
       }
       
       if(inimigo.x>jogador.x){
           inimigo.x--;
       }
       else if(inimigo.x<jogador.x){
-          inimigo.x++;
+          inimigo.x+=1
       }
-      if(inimigo.y<jogador.y){
+      else if(inimigo.x==jogador.x){
+        inimigo.setVelocityX(0);
+      }
+    if(inimigo.y<jogador.y){
           inimigo.y++;
       }
       else if(inimigo.y>jogador.y){
-          inimigo--;
+          inimigo.y--;
       }
-    
 }
 function contadorDeponto(maca,jogador){
-    let i=0; 
-    i++;
-    console.log('i');
-    //destroi();
-    
+    pontuacao+=1;
+        quadroDepontuacao.setText('Pontuação '+pontuacao); 
 }
-function aproximaInimigo(inimigo,tijo){
-    if (inimigo.body.blocked.left){
-        inimigo.body.velocity.x += 10;
+function perde(){
+    console.log('te achei');
+    inimigo.y=Math.random()*700;
+    jogador.x=Math.random()*800;
+    let cont=0;
+    cont++;
+    if(cont==3){
+        quadroDepontuacao.setText("TEMPO ESGOTADO");
+
     }
-    // Se o inimigo encostou em algo na direita, manda-o para esquerda
-    if (inimigo.body.blocked.right){
-        inimigo.body.velocity.x -= 10;
-    
-        }
 }
